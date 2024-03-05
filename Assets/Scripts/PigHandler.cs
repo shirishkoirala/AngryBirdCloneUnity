f1fe0dad29438ffdc0ad943b8e7816edf8cb9135
Shirish Koirala <shirishkoirala@gmail.com>
2024-03-05 21:59:26 +1000

    Animations added

--- Assets/Scripts/PigHandler.cs
+++ Assets/Scripts/PigHandler.cs
@@ -6,20 +6,25 @@ public class Pig : MonoBehaviour
 {
     [SerializeField] private float _madHealth = 3f;
     [SerializeField] private float _damageThreshold = 0.2f;
+    [SerializeField] private  Animator _myAnimator;
+    [SerializeField] private  float _dieAfterSomeTime = 1f;
 
+    private bool _hasDied = false;
     private float _currentHealth;
 
     private void Awake()
     {
         _currentHealth = _madHealth;
     }
-
+    
     public void Damage(float damageAmount)
     {
         _currentHealth -= damageAmount;
+        _myAnimator.SetFloat("Health", _currentHealth);
         if (_currentHealth <= 0f)
         {
-            Die();
+            _dieAfterSomeTime = _myAnimator.GetCurrentAnimatorStateInfo(0).length;
+            StartCoroutine(DieAfterSomeTime());
         }
     }
 
@@ -27,6 +32,12 @@ public class Pig : MonoBehaviour
     {
         GameManager.instance.RemovePig(this);
         Destroy(gameObject);
+    }
+
+    private IEnumerator DieAfterSomeTime()
+    {
+        yield return new WaitForSeconds(_dieAfterSomeTime);
+        Die();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
